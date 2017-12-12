394770021690d931c2f5c0534f1a8362e6f199d4
Matth <matthieu.debuire@gmail.com>
2017-12-12 16:19:13 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+$method = $_server['REQUEST_METHOD'];
+
+if ($method == "POST"){
+$requestBody = file_get_contents('php://input');
+$json = json_decode($requestBody);
+
+$text = $json->result->parameters->text;
+
+switch ($text) {
+case 'Bonjour':
+$speech = "Bonjour, comment vas tu ?"
+break;
+
+case 'Salut':
+$speech = "hey man!"
+break;
+
+default:
+$speech = "Désolé je n'ai pas compris."
+break;
+}
+
+$response = new \stdClass();
+$response->speech = "";
+$response->displayText = "";
+$response->source = "webhook";
+echo json_encode($response);
+}
+else
+{
+echo "method not"
+}
