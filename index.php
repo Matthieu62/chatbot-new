dac01b9091c961ff82fcf5c950f49c74c15a1d9a
Matth <matthieu.debuire@gmail.com>
2017-12-12 17:40:37 +0100

    test

--- index.php
+++ index.php
@@ -3,32 +3,32 @@
 $method = $_server['REQUEST_METHOD'];
 
 if ($method == "POST"){
-$requestBody = file_get_contents('php://input');
-$json = json_decode($requestBody);
+    $requestBody = file_get_contents('php://input');
+    $json = json_decode($requestBody);
 
-$text = $json->result->parameters->text;
+    $text = $json->result->parameters->text;
 
-switch ($text) {
-case 'Bonjour':
-$speech = "Bonjour, comment vas tu ?"
-break;
+    switch ($text) {
+        case 'Bonjour':
+            $speech = "Bonjour, comment vas tu ?"
+        break;
 
-case 'Salut':
-$speech = "hey man!"
-break;
+        case 'Salut':
+            $speech = "hey man!"
+        break;
 
-default:
-$speech = "Désolé je n'ai pas compris."
-break;
-}
+        default:
+            $speech = "Désolé je n'ai pas compris."
+        break;
+    }
 
-$response = new \stdClass();
-$response->speech = "";
-$response->displayText = "";
-$response->source = "webhook";
-echo json_encode($response);
+    $response = new \stdClass();
+    $response->speech = "";
+    $response->displayText = "";
+    $response->source = "webhook";
+    echo json_encode($response);
 }
 else
 {
-echo "method not"
+    echo "method not"
 }
